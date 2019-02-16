b8c898a566bd5a1b7161b81fcd093d1a709b2813
laydervus <laydervus@gmail.com>
2019-02-16 13:51:39 +0000

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Flowreader dark theme
+Dark theme for flowreader.com with greasemonkey or tampermonkey.
+
+Screenshot:
+![](https://i.imgur.com/rMdVPt9.png)
+
+Please make pull requests if you want to add other styles or improve this. (I'm currently using only the Twitter tab).
---
+++ flowreader_dark_theme.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Flowreader dark theme
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Dark theme for flowreader.com
+// @author       https://github.com/layderv
+// @match        https://flowreader.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const interval = 2 * 1000; // refresh colors every <interval> sec
+    const selectors_map = [
+        {name: "body", style: "backgroundColor", color: "#19252e"},
+        {name: "#body", style: "backgroundColor", color: "#19252e"},
+        {name: ".column>.heading", style: "backgroundColor", color: "#19252e"},
+        {name: ".item", style: "backgroundColor", color: "#19252e"},
+        {name: "#top-in", style: "backgroundColor", color: "#19252e"},
+        {name: "#top", style: "backgroundColor", color: "#19252e"},
+        {name: ".readabilityOpened .contentHolder", style: "backgroundColor", color: "#19252e"},
+        {name: "#body.compact .column.primary .item:not(.focused):hover, #body.compact .column.single .item:not(.focused):hover", style: "backgroundColor", color: "#19252e"},
+        {name: ".categories .category .context", style: "backgroundColor", color: "#19252e"},
+        {name: "#body.compact .column.primary .item.rss .heading .title a:after, " +
+         "#body.compact .column.single .item.rss .heading .title a:after, .external-link:after, .icon, .item.rss .body .item-title a:after, [class*=' icon-'], [class^=icon-]", style: "backgroundColor", color: "#19252e"},
+        {name: ".item .body", style: "color", color: "#999"},
+        {name: ".item.rss .body .item-title a", style: "color", color: "#00a3ec"},
+        {name: "#body.compact .column.primary .item:not(.focused) .heading .title a, #body.compact .column.single .item:not(.focused) .heading .title a", style: "color", color: "#7e8c9a"},
+        {name: ".item.twitter .heading .username", style: "color", color: "#0077ad"},
+    ];
+
+    function activate_colors() {
+      for (let sel of selectors_map) {
+        for (let el of document.querySelectorAll(sel.name)) {
+             el.style[sel.style] = sel.color;
+         }
+      }
+    }
+    setInterval(activate_colors, interval);
+    activate_colors();
+})();
+
