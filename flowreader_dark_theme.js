6af5ad44a81c45fa1d788b860e6a116ce696a113
layderv <20249311+layderv@users.noreply.github.com>
2019-03-09 17:06:43 +0000

    Add content page

--- flowreader_dark_theme.js
+++ flowreader_dark_theme.js
@@ -10,7 +10,6 @@
 
 (function() {
     'use strict';
-    const interval = 2 * 1000; // refresh colors every <interval> sec
     const selectors_map = [
         {name: "body", style: "backgroundColor", color: "#19252e"},
         {name: "#body", style: "backgroundColor", color: "#19252e"},
@@ -18,15 +17,20 @@
         {name: ".item", style: "backgroundColor", color: "#19252e"},
         {name: "#top-in", style: "backgroundColor", color: "#19252e"},
         {name: "#top", style: "backgroundColor", color: "#19252e"},
+      	{name: ".column-inner", style: "backgroundColor", color: "#19252e"},
         {name: ".readabilityOpened .contentHolder", style: "backgroundColor", color: "#19252e"},
         {name: "#body.compact .column.primary .item:not(.focused):hover, #body.compact .column.single .item:not(.focused):hover", style: "backgroundColor", color: "#19252e"},
         {name: ".categories .category .context", style: "backgroundColor", color: "#19252e"},
         {name: "#body.compact .column.primary .item.rss .heading .title a:after, " +
          "#body.compact .column.single .item.rss .heading .title a:after, .external-link:after, .icon, .item.rss .body .item-title a:after, [class*=' icon-'], [class^=icon-]", style: "backgroundColor", color: "#19252e"},
+        {name: ".form-control", style: "backgroundColor", color: "#ccc"},
+        {name: ".btn", style: "backgroundColor", color: "#ccc"},
+        
         {name: ".item .body", style: "color", color: "#999"},
         {name: ".item.rss .body .item-title a", style: "color", color: "#00a3ec"},
         {name: "#body.compact .column.primary .item:not(.focused) .heading .title a, #body.compact .column.single .item:not(.focused) .heading .title a", style: "color", color: "#7e8c9a"},
         {name: ".item.twitter .heading .username", style: "color", color: "#0077ad"},
+        {name: ".suggestions .topic .channels .channels-box .addOrRemove", style: "color", color: "#999"},
     ];
 
     function activate_colors() {
@@ -36,7 +40,7 @@
          }
       }
     }
+    const interval = 2 * 1000;
     setInterval(activate_colors, interval);
     activate_colors();
 })();
-
